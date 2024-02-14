3c6db5a1a2109c7435ae41ba09cf74b3cbcc0f88
Tenaka <86342641+Tenaka@users.noreply.github.com>
2024-02-14 07:26:05 +0000

    Add files via upload

---
+++ update xml values.ps1
@@ -0,0 +1,39 @@
+﻿
+$xmlPath = "C:\Program Files\SomeApp\settings.xml"
+
+#AppConfig
+$HideAButton = "true"
+$HideBButton = "false"
+
+#ProfileSettings
+$Resolution = "300"
+
+$xmldoc = new-object System.XML.XMLdocument
+$xmldoc.Load($xmlPath)
+
+$AppConfig = $xmldoc.AppConfig
+$AppConfig.HideAButton = $HideAButton
+$AppConfig.HideBButton = $HideBButton
+
+$ProfileSettings = $xmldoc.AppConfig.ProfileSettings
+$ProfileSettings.Resolution = $Resolution
+
+$xmldoc.Save($xmlPath)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
